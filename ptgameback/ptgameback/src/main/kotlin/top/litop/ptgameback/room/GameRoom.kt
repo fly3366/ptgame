e663468bd806908f2f2cf2e7fcd134dd2377fa68
fly3366 <feiliuxz@hotmail.com>
2019-09-03 10:58:23 +0800

    upload

--- ptgameback/ptgameback/src/main/kotlin/top/litop/ptgameback/room/GameRoom.kt
+++ ptgameback/ptgameback/src/main/kotlin/top/litop/ptgameback/room/GameRoom.kt
@@ -9,10 +9,17 @@ class GameRoom : AbstractVerticle {
   override fun start() {
     var eb = vertx.eventBus()
 
-    var consumer = eb.consumer<Any>("room.gameroom")
+    var consumer = eb.consumer<Any>("room.defalutChannel")
     consumer.handler({ message ->
       println("默认频道收到了信息: ${message.body()}")
-      vertx.deployVerticle("top.litop.ptgameback.room.GameRoom")
+      var oneGame = eb.consumer<Any>("room.defultChannel.oneRoom")
+      consumer.completionHandler({ res ->
+      if (res.succeeded()) {
+        println("默认房间注册成功")
+      } else {
+        println("默认房间注册失败!")
+      }
+    })
     })
     consumer.completionHandler({ res ->
       if (res.succeeded()) {
