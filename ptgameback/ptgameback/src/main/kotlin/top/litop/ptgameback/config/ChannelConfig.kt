00f3c9fd355b062114272912ede9bd6d639cad0c
fly3366 <feiliuxz@hotmail.com>
2019-09-06 22:05:34 +0800

    Update Channel

--- ptgameback/ptgameback/src/main/kotlin/top/litop/ptgameback/config/ChannelConfig.kt
+++ ptgameback/ptgameback/src/main/kotlin/top/litop/ptgameback/config/ChannelConfig.kt
@@ -9,21 +9,31 @@ class ChannelConfig {
   class RoomConfig {
     public val roomId: String
     public var roomName: String
-    public var eventLine:GameChannel
-    constructor(roomId: String, roomName: String,eventLine:GameChannel) {
+    public var eventLine: GameChannel
+
+    constructor(roomId: String, roomName: String, eventLine: GameChannel) {
       this.roomId = roomId
       this.roomName = roomName
       this.eventLine = eventLine
       var eb = eventLine.vertx.eventBus()
-      eb.publish("room.defaultChannel", roomId+"房间:"+roomName+"被频道"+eventLine.getGameChannelConfig()+"创建")
+      eb.publish("room." + eventLine.getGameChannelConfig() + "Channel", roomId + "房间:" + roomName + "被频道" + eventLine.getGameChannelConfig() + "创建")
     }
   }
 
-  private var gameRoomList = mutableMapOf<String,RoomConfig>()
+  private var gameRoomList: MutableMap<String, RoomConfig>
 
-  constructor(channelId: String, channelName: String,eventLine:GameChannel) {
+  public fun getGameRoomList(): MutableMap<String, RoomConfig> {
+    return this.gameRoomList
+  }
+
+  public fun setGameRoomList(roomId: String, roomConfig: RoomConfig) {
+    this.gameRoomList.put(roomId, roomConfig)
+  }
+
+
+  constructor(channelId: String, channelName: String, eventLine: GameChannel) {
     this.channelId = channelId
     this.channelName = channelName
-    this.gameRoomList.put("World",RoomConfig("0","World",eventLine))
+    this.gameRoomList = mutableMapOf<String, RoomConfig>()
   }
 }
--- ptgameback/ptgameback/src/main/kotlin/top/litop/ptgameback/room/GameChannel.kt
+++ ptgameback/ptgameback/src/main/kotlin/top/litop/ptgameback/room/GameChannel.kt
@@ -5,42 +5,63 @@ import top.litop.ptgameback.config.ChannelConfig
 
 class GameChannel : AbstractVerticle {
   lateinit var gameChannelConfig: ChannelConfig
-  var isReady:Boolean
+  var isReady: Boolean
+  lateinit var gameEventMap: MutableMap<String, MessageConsumer<Any>>
+
   constructor() {
     this.isReady = false
+
   }
 
-  fun getGameChannelConfig():String{
-    if(this.isReady)
+  fun getGameChannelConfig(): String {
+    if (this.isReady)
       return this.gameChannelConfig.channelId
     else
-      return "初始化的"
+      return "Initlizing"
   }
 
 
   private fun channelInit(): Boolean {
-    this.gameChannelConfig = ChannelConfig("A001", "Default",this)
+    this.gameChannelConfig = ChannelConfig("A001", "Default", this)
+    this.gameEventMap = mutableMapOf()
     return true
   }
 
-  override fun start() {
-    var eb = vertx.eventBus()
-
-    var consumer = eb.consumer<Any>("room.defaultChannel")
-    consumer.handler({ message ->
-      println("默认频道收到了信息: ${message.body()}")
-
-    })
-    consumer.completionHandler({ res ->
+  private fun gameRoomCreate(roomId: String, roomName: String) {
+    var eb = this.vertx.eventBus()
+    var gameRoomEventLine = eb.consumer<Any>("room." + this.getGameChannelConfig() + "Channel." + roomId)
+    gameRoomEventLine.completionHandler({ res ->
       if (res.succeeded()) {
-        if (this.channelInit()) {
-          println(this.gameChannelConfig.channelId + "频道注册成功")
-          this.isReady=true
-        } else
-          println("默认频道注册失败!")
+        this.gameChannelConfig.setGameRoomList(roomId, ChannelConfig.RoomConfig(roomId, roomName, this))
+        println(this.getGameChannelConfig() + "频道" + roomId + "房间注册成功")
       } else {
-        println("默认频道注册失败!")
+        println(this.getGameChannelConfig() + "频道" + roomId + "房间注册失败!")
       }
     })
+  }
+
+  override fun start() {
+    var eb = this.vertx.eventBus()
+    if (this.channelInit()) {
+      this.isReady = true
+    } else {
+      println(this.getGameChannelConfig() + "频道初始化失败")
+    }
+    var channelMessage = eb.consumer<Any>("room." + this.getGameChannelConfig() + ".Channel")
+    channelMessage.handler({ message ->
+      println(this.getGameChannelConfig() + "频道收到了信息: ${message.body()}")
+    })
+    channelMessage.completionHandler({ res ->
+      if (res.succeeded()) {
+        println(this.getGameChannelConfig() + "频道注册成功")
+        this.gameRoomCreate("0", "World")
+      } else {
+        println(this.getGameChannelConfig() + "频道注册失败!")
+      }
+    })
+  }
+
+  override fun stop() {
+//    println(this.getGameChannelConfig() + "频道注册失败!")
   }
 }
