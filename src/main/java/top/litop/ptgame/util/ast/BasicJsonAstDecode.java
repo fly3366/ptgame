90db91768094c8c251a2ef7fb55867e40fa48641
fly3366 <feiliuxz@hotmail.com>
2019-09-24 21:51:03 +0800

    JSON DECODE UPDATE

--- src/main/java/top/litop/ptgame/util/ast/BasicJsonAstDecode.java
+++ src/main/java/top/litop/ptgame/util/ast/BasicJsonAstDecode.java
@@ -1,7 +1,13 @@
 package top.litop.ptgame.util.ast;
 
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class BasicJsonAstDecode implements AstDecode {
 
@@ -11,8 +17,47 @@ public class BasicJsonAstDecode implements AstDecode {
         final JsonObject initJsonObj = (JsonObject) initValue;
 
 
-        Ast localAst = new BasicAst();
+        BasicAst localAst = new BasicAst();
 
+        Map<String, String> clientSource = new HashMap<String, String>();
+
+        Iterator<Map.Entry<String, Object>> clientValue = initJsonObj.getJsonObject("clientValue").iterator();
+        while (clientValue.hasNext()) {
+            Map.Entry<String, Object> next = clientValue.next();
+            clientSource.put(next.getKey(), next.getValue().toString());
+        }
+        localAst.setClientDisplaySource(clientSource);
+
+
+        JsonArray initProps = initJsonObj.getJsonArray("initProps");
+        Map<String, Integer> playerProps[] =
+                new HashMap[initProps.size()];
+
+        Iterator<Object> initPropsValue = initProps.iterator();
+        int initPropsFlag = 0;
+        while (initPropsValue.hasNext()) {
+            JsonObject next = (JsonObject) initPropsValue.next();
+            Iterator<Map.Entry<String, Object>> onePlayerProp = next.iterator();
+            while (onePlayerProp.hasNext()) {
+                Map.Entry<String, Object> onePlayerOneProp = onePlayerProp.next();
+                playerProps[initPropsFlag].put(onePlayerOneProp.getKey(), Integer.parseInt(onePlayerOneProp.getValue().toString()));
+            }
+            initPropsFlag++;
+        }
+        localAst.setClientPlayerProps(playerProps);
+
+
+        JsonArray nextAstFork = initJsonObj.getJsonArray("nextStage");
+
+        String clientFork[] = new String[nextAstFork.size()];
+        Iterator<Object> nextAstForkIterator = nextAstFork.iterator();
+        int nextAstForkFlag = 0;
+        while (nextAstForkIterator.hasNext()) {
+            JsonObject next = (JsonObject) nextAstForkIterator.next();
+            clientFork[nextAstForkFlag] = next.encode();
+            nextAstForkFlag++;
+        }
+        localAst.setClientFork(clientFork);
         return null;
     }
 }
