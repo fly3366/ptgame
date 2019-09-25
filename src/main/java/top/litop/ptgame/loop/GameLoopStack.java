b1f8f834724ff4144e000089701230517ca55927
fly3366 <feiliuxz@hotmail.com>
2019-09-25 10:48:30 +0800

    UPDATE GAME LOOP AND STACK

--- src/main/java/top/litop/ptgame/loop/GameLoopStack.java
+++ src/main/java/top/litop/ptgame/loop/GameLoopStack.java
@@ -1,5 +1,35 @@
 package top.litop.ptgame.loop;
 
-public class GameLoopStack {
+import top.litop.ptgame.util.ast.AstTree;
+import top.litop.ptgame.util.ast.BasicJsonAstDecode;
+import top.litop.ptgame.util.swtich.BasicSwitcher;
 
+public class GameLoopStack {
+    private PlayerProp[] playersProp;
+
+    private AstTree masterTree;
+
+    private AstTree localAstTree;
+
+    private AstTree mayNextAstTree;
+
+    public GameLoopStack(AstTree localAstTree) {
+        this.localAstTree = localAstTree;
+    }
+
+    ;
+
+    public AstTree getLocalAstTree() {
+        return localAstTree;
+    }
+
+    public void gotoNext() {
+        if (this.localAstTree.getNextAstValues().length == 0) {
+            //END
+        } else {
+            this.localAstTree = BasicJsonAstDecode.astTreeDecode(
+                    localAstTree.getNextAstValues()[BasicSwitcher.staticSwitch(localAstTree)]
+            );
+        }
+    }
 }
